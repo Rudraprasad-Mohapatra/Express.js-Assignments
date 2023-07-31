b53f5c749946037e7ee2682b9f6a763bd3d52525
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-01 00:03:00 +0530

    Server has started Successfully

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = req('express');
+const express = require('express');
+const PORT = 5500;
 const app = express();
+
 
 app.listen(PORT, (req, res)=> {
     console.log(` Server listening at http://127.0.0.1:${PORT}`);
