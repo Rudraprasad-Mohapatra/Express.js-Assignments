c7112b09ee96b4e1464d2f8488cb6cef969c7067
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-03 19:58:24 +0530

    Removing body-parser module from app2.js

--- app2.js
+++ app2.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const bodyParser = require("express");
 const app2 = express();
 const appRouter2 = require("./router/appRouter2.js");
-app2.use(bodyParser.json());
 app2.use(express.json());
 app2.use(express.urlencoded({extended: true}));
 app2.use("/", appRouter2);
