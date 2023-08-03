549110214899ab3bd7ffffd9c142b77e5e019696
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-03 20:41:40 +0530

    Adding basic information in index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Q1 is app.js, Q2 is app2.js, Q3 is app3.js, Q4 is app4.js
+
+// Please start the server by the command "npm start" and test the urls as per mentioned in the questions.
+
 const app = require("./app.js");
 const app2 = require("./app2.js");
 const app3 = require("./app3.js");
